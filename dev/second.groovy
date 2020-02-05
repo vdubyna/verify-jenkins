d2d2a189616c914d94f2144289e62c0a3d93aeb4
Volodymyr <vovikha@gmail.com>
2020-02-05 19:02:20 +0200

    Update second.groovy

--- dev/second.groovy
+++ dev/second.groovy
@@ -1,8 +1,8 @@
-def test1(){
+def test1(modules){
     echo "second groovy 1"
     modules.first.test1()
 }
-def test2(){
+def test2(modules){
     echo "second groovy 2"
     modules.first.test2()
 }
