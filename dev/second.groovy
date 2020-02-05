7e433f770986955a6da783b59264e89889af0a6d
Volodymyr <vovikha@gmail.com>
2020-02-05 18:21:25 +0200

    Update second.groovy

--- dev/second.groovy
+++ dev/second.groovy
@@ -1,7 +1,11 @@
+Map modules = [:]
+modules.first = load './../first.groovy'
 def test1(){
     echo "second groovy 1"
+    modules.first.test1()
 }
 def test2(){
     echo "second groovy 2"
+    modules.first.test2()
 }
 return this
